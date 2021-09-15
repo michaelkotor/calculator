35fed819d42800bc4e5abd259df25412f462596c
michaelkotor <milan3020@gmail.com>
2021-09-15 20:47:05 +0300

    second

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.row_parent}/>
+      <View style={styles.row_parent}>
+          <Button title="AC" style={styles.row} />
+          <Button title="+/-" style={styles.row} />
+          <Button title="%" style={styles.row} />
+          <Button title="/" style={styles.row} />
+
+      </View>
+      <View style={styles.row_parent}>
+        <Button title="7"/>
+        <Button title="8"/>
+        <Button title="9"/>
+        <Button title="*"/>
+      </View>
+      <View style={styles.row_parent}>
+        <Button title="4"/>
+        <Button title="5"/>
+        <Button title="6"/>
+        <Button title="-"/>
+      </View>
+      <View style={styles.row_parent}>
+        <Button title="1"/>
+        <Button title="2"/>
+        <Button title="3"/>
+        <Button title="+"/>
+      </View>
+      <View style={styles.row_parent}>
+        <Button title="0"/>
+        <Button title="."/>
+        <Button title="="/>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "blue",
+    },
+    row_parent: {
+        flex: 1,
+        backgroundColor: "gold",
+        flexDirection: "row",
+        // alignItems: "center"
+        justifyContent: "center",
+        borderColor: "red",
+        alignItems: "center",
+        width: "100%"
+    },
+    row: {
+        flexGrow: 1,
+        alignSelf: "center"
+    }
 });
+
+
