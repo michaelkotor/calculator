b896eed8e9b2962024909770ff40d996c8fb118a
michaelkotor <milan3020@gmail.com>
2021-09-17 19:54:42 +0300

    calculator ready

--- App.js
+++ App.js
@@ -1,64 +1,140 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from "react";
+import {StyleSheet, Text, View} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-        <Text style={styles.row_parent}/>
-      <View style={styles.row_parent}>
-          <Button title="AC" style={styles.row} />
-          <Button title="+/-" style={styles.row} />
-          <Button title="%" style={styles.row} />
-          <Button title="/" style={styles.row} />
+import Calc from 'expression-calculator';
 
-      </View>
-      <View style={styles.row_parent}>
-        <Button title="7"/>
-        <Button title="8"/>
-        <Button title="9"/>
-        <Button title="*"/>
-      </View>
-      <View style={styles.row_parent}>
-        <Button title="4"/>
-        <Button title="5"/>
-        <Button title="6"/>
-        <Button title="-"/>
-      </View>
-      <View style={styles.row_parent}>
-        <Button title="1"/>
-        <Button title="2"/>
-        <Button title="3"/>
-        <Button title="+"/>
-      </View>
-      <View style={styles.row_parent}>
-        <Button title="0"/>
-        <Button title="."/>
-        <Button title="="/>
-      </View>
+import InputNumberButton from "./InputNumberButton.js";
 
-    </View>
-  );
+const buttons = [
+    ["CLEAR", "DEL"],
+    ["7", "8", "9", "/"],
+    ["4", "5", "6", "*"],
+    ["1", "2", "3", "-"],
+    ["0", ".", "=", "+"]
+];
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.initialState = {
+            displayValue: "0",
+        };
+        this.state = this.initialState;
+    }
+
+    renderButtons() {
+        return buttons.map((buttonRows, index) => {
+            let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
+                return (
+                    <InputNumberButton
+                        value={buttonItems}
+                        handleOnPress={this.handleInput.bind(this, buttonItems)}
+                        key={"btn-" + buttonIndex}
+                    />
+                );
+            });
+
+            return (
+                <View style={styles.inputRow} key={"row-" + index}>
+                    {rowItem}
+                </View>
+            );
+        });
+    }
+
+    handleInput = input => {
+        const {
+            displayValue
+        } = this.state;
+
+        switch (input) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                if (displayValue === "0") {
+                    this.setState({displayValue: input});
+                } else {
+                    this.setState({displayValue: displayValue + input});
+                }
+                break;
+
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if(displayValue[displayValue.length - 1] === '.') {
+                    break;
+                }
+                if(!isNaN(displayValue[displayValue.length - 1])) {
+                    this.setState({displayValue: displayValue + input});
+                } else {
+                    this.setState({displayValue: displayValue.slice(0, -1) + input});
+                }
+                break;
+            case ".":
+                if(!isNaN(displayValue[displayValue.length - 1])) {
+                    this.setState({displayValue: displayValue + input});
+                }
+                break;
+            case "=":
+                const calc = new Calc(displayValue);
+                const result = calc.calc();
+                this.setState({displayValue: result + ""});
+                break;
+            case "CLEAR":
+                this.setState({displayValue: "0"});
+                break;
+            case "DEL":
+                if (displayValue.length === 1) {
+                    this.setState({displayValue: "0"});
+                } else {
+                    this.setState({displayValue: displayValue.slice(0, -1)});
+                }
+                break;
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.resultContainer}>
+                    <Text style={styles.resultText}>{this.state.displayValue}</Text>
+                </View>
+                <View style={styles.inputContainer}>{this.renderButtons()}</View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "blue",
+        flex: 1
     },
-    row_parent: {
-        flex: 1,
-        backgroundColor: "gold",
-        flexDirection: "row",
-        // alignItems: "center"
-        justifyContent: "center",
-        borderColor: "red",
-        alignItems: "center",
-        width: "100%"
+    resultContainer: {
+        flex: 2,
+        backgroundColor: "#373737",
+        justifyContent: "center"
     },
-    row: {
-        flexGrow: 1,
-        alignSelf: "center"
+    inputContainer: {
+        flex: 8,
+        backgroundColor: "#535353"
+    },
+    resultText: {
+        color: "white",
+        fontSize: 80,
+        fontWeight: "bold",
+        padding: 20,
+        textAlign: "right"
+    },
+    inputRow: {
+        flex: 1,
+        flexDirection: "row"
     }
 });
-
-
